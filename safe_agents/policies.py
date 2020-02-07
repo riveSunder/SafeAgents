76a99bf2ab120b8bead0331e0f3a15de431c0986
Rive Sunder <rivesunder@protonmail.com>
2020-02-07 12:09:17 +0100

    add multivariate normal param initialization to MLP policy

--- safe_agents/policies.py
+++ safe_agents/policies.py
@@ -22,6 +22,15 @@ class MLP():
         self.hid_dim = hid_dim
         self.act = act
         
+        self.num_parameters = self.input_dim * self.hid_dim[0]
+        self.num_parameters += self.hid_dim[-1] * self.output_dim
+        for hid_idx in range(1, len(hid_dim)):
+            self.num_parameters += self.hid_dim[hid_idx-1] \
+                    * self.hid_dim[hid_idx]
+
+        self.mean = np.zeros((self.num_parameters))
+        self.covar =  np.eye(self.num_parameters)
+
         self.init_params()
 
     def forward(self, x):
@@ -36,31 +45,43 @@ class MLP():
             
         x = np.matmul(x, self.w_h2y) + self.b_h2y
 
-        return y
+        return x
 
     def init_params(self):
+        
+        self.parameters = np.random.multivariate_normal(self.mean, self.covar)
 
-        self.w_x2h = np.random.randn(self.input_dim, self.hid_dim[0]) \
-                / self.input_dim
+        start = 0
+        end = self.input_dim * self.hid_dim[0]
+        self.w_x2h = self.parameters[start:end]\
+                .reshape(self.input_dim, self.hid_dim[0])
         self.b_x2h = np.zeros((1,self.hid_dim[0]))
+
+        start = end
 
         self.w_hid = {}
         self.b_hid = {}
         for hid_idx in range(1,len(self.hid_dim)):
-            self.w_hid[hid_idx] = np.random.randn(\
-                    self.hid_dim[hid_idx-1], self.hid_dim[hid_idx]\
-                    )\
-                    / self.hid_dim[hid_idx-1]
+            end = start + self.hid_dim[hid_idx-1] * self.hid_dim[hid_idx]
+        
 
+            self.w_hid[hid_idx] = self.parameters[start:end]\
+                    .reshape(self.hid_dim[hid_idx-1], self.hid_dim[hid_idx])
+
+            start = end
+            
             self.b_hid[hid_idx] = np.zeros((1,self.hid_dim[hid_idx]))
 
-        self.w_h2y = np.random.randn(self.hid_dim[-1],self.output_dim)\
-                / self.hid_dim[-1]
-        self.b_h2y = np.zeros((1, self.outout_dim)))
+        end = start + self.hid_dim[-1] * self.output_dim
+
+        self.w_h2y = self.parameters[start:end]\
+                .reshape(self.hid_dim[-1], self.output_dim)
+        self.b_h2y = np.zeros((1, self.output_dim))
 
 
 if __name__ == "__main__":
 
     policy = MLP(4,1,[32,32])
 
-
+    x = np.random.randn(100,4)
+    y = policy.forward(x)
