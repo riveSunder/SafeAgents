3ae931f7a99fd115410888d1664d0d1c3be28bae
Rive Sunder <rivesunder@protonmail.com>
2020-02-10 20:00:04 +0100

    tweak cost constraints

--- safe_agents/train.py
+++ safe_agents/train.py
@@ -17,9 +17,9 @@ def get_fitness(agent, env, epds, get_cost=True):
         sum_cost = 0
         obs = env.reset()
 
-        while not done and steps < 500:
+        while not done and steps < 1000:
             action = agent.forward(obs)
-            if len(action.shape) >1:
+            if len(action.shape) > 1:
                 action = action.squeeze()
             
             obs, reward, done, info = env.step(action)
@@ -33,15 +33,15 @@ def get_fitness(agent, env, epds, get_cost=True):
 
     return sum_reward, sum_cost
 
-def get_elite_mean(population, reward, cost=None):
+def get_elite_mean(population, reward, cost=None,cost_constraint=2.5):
 
 
-    
     if cost is not None:
-        cost_fitness_agent = [[-cost, fit, agent.parameters]
-                for cost, fit, agent in \
-                sorted(zip(cost, reward, population),\
-                key = lambda trip: [-trip[0], trip[1]], reverse=True)]
+        adjusted_cost = [max([cost_constraint, elem]) for elem in cost]
+        cost_fitness_agent = [[cost, fit, agent.parameters]
+                for a_cost, cost, fit, agent in \
+                sorted(zip(adjusted_cost, cost, reward, population),\
+                key = lambda trip: [-trip[0], trip[2]], reverse=True)]
             
         fitness = [elem[1] for elem in cost_fitness_agent]
         cost = [elem[0] for elem in cost_fitness_agent]
@@ -61,10 +61,10 @@ def get_elite_mean(population, reward, cost=None):
     elite_fitness = fitness[:keep]
 
     print("population mean cost, rewards: {:.3e}, {:.3e}".format(\
-            -np.mean(cost), np.mean(fitness)))
+            np.mean(cost), np.mean(fitness)))
 
     print("elite mean cost, rewards: {:.3e}, {:.3e}".format(\
-            -np.mean(elite_cost), np.mean(elite_fitness)))
+            np.mean(elite_cost), np.mean(elite_fitness)))
 
     param_sum = elite_pop[0]
 
@@ -74,14 +74,17 @@ def get_elite_mean(population, reward, cost=None):
 
     param_means = param_sum / keep
 
-    return param_means
+    return [param_means, np.mean(cost), np.mean(elite_cost), \
+            np.mean(fitness), np.mean(elite_fitness)]
 
 def train_es(env, input_dim, output_dim, pop_size=6, max_gen=100, cost_constraint=0.0, cost_penalty=False):
 
     # hard-coded policy parameters
     hid_dim = [32,32]
+    es_lr = 1e-1
 
 
+    
     # generate a population
     population = [MLP(input_dim, output_dim, hid_dim) \
             for ii in range(pop_size)]
@@ -90,23 +93,51 @@ def train_es(env, input_dim, output_dim, pop_size=6, max_gen=100, cost_constrain
 #    for ll in range(pop_size):
 #        population[ll].mean = param_means 
 #        population[ll].init_params() 
-    
-    for gen in range(max_gen):
-        fitnesses = []
-        costs = []
-        for agent_idx in range(len(population)):
+    results = {"costs": [],
+            "rewards": [],
+            "elite_costs": [],
+            "elite_rewards": []}
+    try:
+        for gen in range(max_gen):
+            fitnesses = []
+            costs = []
+            print("generation {}".format(gen))
 
-            reward, cost = get_fitness(population[agent_idx], env, epds=16)
-            fitnesses.append(reward)
-            costs.append(cost)
-        
-        costs = [max([cost_constraint, elem]) for elem in costs]
-        param_means = get_elite_mean(population, fitnesses, costs)
+            for agent_idx in range(len(population)):
 
-        for ll in range(pop_size):
-            population[ll].mean = param_means 
-            population[ll].init_params() 
-        
+                reward, cost = get_fitness(population[agent_idx], env, epds=2)
+                fitnesses.append(reward)
+                costs.append(cost)
+            
+            try:
+                param_means *= (1 - es_lr)
+                gen_results = get_elite_mean(population, fitnesses, costs, cost_constraint)
+                param_means += es_lr * gen_results[0] 
+            except:
+                gen_results = get_elite_mean(population, fitnesses, costs, cost_constraint)
+                param_means = gen_results[0] 
+
+            for ll in range(pop_size):
+                population[ll].mean = param_means 
+                population[ll].init_params() 
+            
+            results["costs"].append(gen_results[1])
+            results["elite_costs"].append(gen_results[2])
+            results["rewards"].append(gen_results[3])
+            results["elite_rewards"].append(gen_results[4])
+
+            if gen % 5 == 0:
+                np.save("./means_gen{}.npy".format(gen), param_means)
+                np.save("./temp_results.npy", results)
+            del fitnesses
+            del costs
+
+    except KeyboardInterrupt:
+        pass
+
+
+    np.save("./means_gen{}.npy".format(gen), param_means)
+    np.save("./temp_results.npy", results)
     import pdb; pdb.set_trace()
 
 
@@ -126,7 +157,7 @@ if __name__ == "__main__":
         obs_dim = env.observation_space.sample().shape[0]
         act_dim = env.action_space.sample().shape[0]
 
-        train_es(env, obs_dim, act_dim, cost_constraint=2.5, pop_size=64, max_gen=1024)
+        train_es(env, obs_dim, act_dim, cost_constraint=25, pop_size=128, max_gen=1024)
         
         print("all oK")
 
