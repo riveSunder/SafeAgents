23c60f19721e5e33afd0496881f0f6c3ab736377
Rive Sunder <rivesunder@protonmail.com>
2020-03-03 19:44:53 +0100

    add more parseargs options, ability to resume training, kart env training option

--- safe_agents/train.py
+++ safe_agents/train.py
@@ -6,6 +6,7 @@ import time
 from safe_agents.policies import MLP
 from open_safety_gym.envs.puck_env import PuckEnv
 from open_safety_gym.envs.balance_bot_env import BalanceBotEnv 
+from open_safety_gym.envs.kart_env import KartEnv
 
 def get_fitness(agent, env, epds, get_cost=True, max_steps=1000):
 
@@ -80,32 +81,39 @@ def get_elite_mean(population, reward, cost=None,cost_constraint=2.5):
     return [param_means, np.mean(cost), np.mean(elite_cost), \
             np.mean(fitness), np.mean(elite_fitness)]
 
-def train_es(env, input_dim, output_dim, pop_size=6, max_gen=100, cost_constraint=0.0, cost_penalty=False):
+def train_es(env, input_dim, output_dim, pop_size=6, max_gen=100, cost_constraint=0.0, cost_penalty=False,\
+        model=None, reward_hypothesis=False):
 
     # hard-coded policy parameters
     hid_dim = [32,32]
     es_lr = 1e-1
-
-
+    reward_cost_ratio = 20
     
     # generate a population
     population = [MLP(input_dim, output_dim, hid_dim) \
             for ii in range(pop_size)]
-#    param_means = np.load("./safe_agents/results/means_gen995.npy")
-#
-#    for ll in range(pop_size):
-#        population[ll].mean = param_means 
-#        population[ll].init_params() 
+    
+    if model is not None:
+        param_means = np.load(model)
+
+        for ll in range(pop_size):
+            population[ll].mean = param_means 
+            population[ll].init_params() 
+
     results = {"costs": [],
+            "combined_rc": [],
             "rewards": [],
             "elite_costs": [],
             "elite_rewards": [], 
             "steps": []}
+    
     try:
         for gen in range(max_gen):
             fitnesses = []
             costs = []
             total_steps = []
+            rc_fitnesses = []
+            print("generation {}".format(gen))
 
             for agent_idx in range(len(population)):
                 max_steps = 2000 #np.min([2000, 250 + 10* gen])
@@ -115,13 +123,25 @@ def train_es(env, input_dim, output_dim, pop_size=6, max_gen=100, cost_constrain
                 fitnesses.append(reward)
                 total_steps.append(steps)
                 costs.append(cost)
+                rc_fitnesses.append(reward - cost / reward_cost_ratio)
             
             try:
                 param_means *= (1 - es_lr)
-                gen_results = get_elite_mean(population, fitnesses, costs, cost_constraint)
+
+                if reward_hypothesis:
+                    cost_constraint = float("Inf")
+                    gen_results = get_elite_mean(population, rc_fitnesses, costs, cost_constraint)
+                else:
+                    gen_results = get_elite_mean(population, fitnesses, costs, cost_constraint)
+
                 param_means += es_lr * gen_results[0] 
             except:
-                gen_results = get_elite_mean(population, fitnesses, costs, cost_constraint)
+
+                if reward_hypothesis:
+                    gen_results = get_elite_mean(population, rc_fitnesses, costs, cost_constraint)
+                else:
+                    gen_results = get_elite_mean(population, fitnesses, costs, cost_constraint)
+
                 param_means = gen_results[0] 
 
             for ll in range(pop_size):
@@ -164,25 +184,45 @@ if __name__ == "__main__":
     parser.add_argument("-a", "--algo", type=str,\
             help="training algo", default="es")
     parser.add_argument("-c", "--constraint", type=float,\
-            help="safety constraint", default=10.0)
+            help="safety constraint", default=9e9)
+
+    parser.add_argument("-m", "--model", type=str,\
+            help="resume parameters stored at filepath (default None)", default=None)
+    parser.add_argument("-r", "--reward_hypothesis", type=bool,\
+            help="combine cost and reward (reward hypothesis)", default=False)
+    parser.add_argument("-p", "--pop_size",type=int,\
+            help="population size", default=64)
 
     args = parser.parse_args()
 
     constraint = args.constraint
+    rh = args.reward_hypothesis
+    model = args.model
+    pop_size = args.pop_size
+
     if "uck" in args.env_name:
         env = PuckEnv(render=False)
         obs_dim = env.observation_space.sample().shape[0]
         act_dim = env.action_space.sample().shape[0]
 
-        train_es(env, obs_dim, act_dim, cost_constraint=constraint, pop_size=64, max_gen=2048)
+        train_es(env, obs_dim, act_dim, cost_constraint=constraint, pop_size=pop_size, max_gen=2048, \
+                model=model, reward_hypothesis=rh)
         
     elif "alance" in args.env_name:
         env = BalanceBotEnv(render=False)
         obs_dim = env.observation_space.sample().shape[0]
         act_dim = env.action_space.sample().shape[0]
 
-        train_es(env, obs_dim, act_dim, cost_constraint=constraint, pop_size=64,\
-                max_gen=2048)
+        train_es(env, obs_dim, act_dim, cost_constraint=constraint, pop_size=pop_size,\
+                max_gen=2048, model=model, reward_hypothesis=rh)
+
+    elif "art" in args.env_name:
+        env = KartEnv(render=False)
+        obs_dim = env.observation_space.sample().shape[0]
+        act_dim = env.action_space.sample().shape[0]
+
+        train_es(env, obs_dim, act_dim, cost_constraint=constraint, pop_size=pop_size,\
+                max_gen=2048, model=model, reward_hypothesis=rh)
         
     print("all oK")
 
